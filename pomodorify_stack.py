24336abd7d8de9c79c6db46b25ef63d8fc38770b
Scot Kennedy <gateless@gmail.com>
2025-06-06 15:29:04 -0700

    switch to CDK

--- pomodorify_stack.py
+++ pomodorify_stack.py
@@ -18,15 +18,18 @@ class PomodorifyStack(Stack):
             code=_lambda.Code.from_asset("backend"),
             handler="wsgi_handler.handler",
             environment={
-                "SPOTIFY_CLIENT_ID": ssm.StringParameter.value_for_string_parameter(
-                    self, "/pomodorify/dev/SPOTIFY_CLIENT_ID"
-                ),
-                "SPOTIFY_CLIENT_SECRET": ssm.StringParameter.value_for_string_parameter(
-                    self, "/pomodorify/dev/SPOTIFY_CLIENT_SECRET"
-                ),
-                "SPOTIFY_REDIRECT_URI": ssm.StringParameter.value_for_string_parameter(
-                    self, "/pomodorify/dev/SPOTIFY_REDIRECT_URI"
-                ),
+                "SPOTIFY_CLIENT_ID": ssm.StringParameter.from_string_parameter_name(
+                    self, "SpotifyClientId",
+                    "/pomodorify/dev/SPOTIFY_CLIENT_ID"
+                ).string_value,
+                "SPOTIFY_CLIENT_SECRET": ssm.StringParameter.from_string_parameter_name(
+                    self, "SpotifyClientSecret",
+                    "/pomodorify/dev/SPOTIFY_CLIENT_SECRET"
+                ).string_value,
+                "SPOTIFY_REDIRECT_URI": ssm.StringParameter.from_string_parameter_name(
+                    self, "SpotifyRedirectUri",
+                    "/pomodorify/dev/SPOTIFY_REDIRECT_URI"
+                ).string_value,
             }
         )
 

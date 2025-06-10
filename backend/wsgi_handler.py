969ce6a321f45e5432f268291b4530cc4f7439be
Scot Kennedy <gateless@gmail.com>
2025-06-10 12:50:22 -0700

    some fixes

--- backend/wsgi_handler.py
+++ backend/wsgi_handler.py
@@ -6,6 +6,5 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from handler import app
 
-def handler(event, context):
-    from serverless_wsgi import handle_request
-    return handle_request(app, event, context) 
+# This is the WSGI application that Gunicorn will use
+application = app 
